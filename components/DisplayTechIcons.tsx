e31df5382a6b41f400a55c593cfc5ec67b56bf3d
Mudita Jain <mudita21jain@gmail.com>
2025-04-05 23:35:20 +0530

    `Update DisplayTechIcons component to use cn utility function`

--- components/DisplayTechIcons.tsx
+++ components/DisplayTechIcons.tsx
@@ -1,4 +1,4 @@
-import { getTechLogos } from "@/lib/utils";
+import { cn, getTechLogos } from "@/lib/utils";
 import Image from "next/image";
 import React from "react";
 
@@ -6,7 +6,7 @@ const DisplayTechIcons = async({ techStack }: TechIconProps) => {
     const techIcons = await getTechLogos(techStack);
     return (
         <div className="flex flex-row">{techIcons.slice(0,3).map(({tech, url},index)=>(
-            <div key={tech} className="relative group bg-dark-300 rounded-full p-2 flex-center">
+            <div key={tech} className={cn("relative group bg-dark-300 rounded-full p-2 flex-center", index>=1 && "-ml-3")}>
                 <span className="tech-tooltip">{tech}</span>
                 <Image src={url} alt="{tech}" width={20} height={20} className="size-5" />
             </div>
