241973de0d78d3ba4311f09624e064ece80d50ed
Mudita Jain <mudita21jain@gmail.com>
2025-04-06 18:45:30 +0530

    `Update Firebase configuration to use environment variables`

--- firebase/client.ts
+++ firebase/client.ts
@@ -1,17 +1,18 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAb3P5Ds7qaU3kPBZILfEDAGDMxObnkyTk",
-  authDomain: "jobsy-4e401.firebaseapp.com",
-  projectId: "jobsy-4e401",
-  storageBucket: "jobsy-4e401.firebasestorage.app",
-  messagingSenderId: "524909520863",
-  appId: "1:524909520863:web:2f0834e4d735b9e984866a",
-  measurementId: "G-4PMQ250ZBX"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-const app =!getApps.length? initializeApp(firebaseConfig): getApp();
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 export const auth = getAuth(app);
 export const db = getFirestore(app);
